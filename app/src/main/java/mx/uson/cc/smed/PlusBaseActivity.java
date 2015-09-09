4e4b119370f5b84b69909f844f8731c5064d3617
Nan Montaño <nano.mv112@gmail.com>
2015-09-09 08:25:18 -0700

    src

---
+++ app/src/main/java/mx/uson/cc/smed/PlusBaseActivity.java
@@ -0,0 +1,116 @@
+package mx.uson.cc.smed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.plus.Plus;
+
+/**
+ * A base class to wrap communication with the Google Play Services PlusClient.
+ */
+public abstract class PlusBaseActivity extends AppCompatActivity
+        implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        View.OnClickListener {
+
+    private static String TAG = PlusBaseActivity.class.toString();
+    private static final int RC_SIGN_IN = 0;
+    private GoogleApiClient mGoogleApiClient;
+
+
+    /** Is there a ConnectionResult solution in progress? */
+    private boolean mIsResolving = false;
+    /** Should we automatically resolve ConnectionResults when possible?*/
+    private boolean mShouldResolve = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API)
+                .addScope(new Scope(Scopes.PROFILE))
+                .build();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult: " + requestCode + ":" + resultCode
+                + ":" + data);
+        if(requestCode == RC_SIGN_IN){
+            if(resultCode != RESULT_OK){
+                mShouldResolve = false;
+            }
+            mIsResolving = false;
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailes:" + connectionResult);
+        if(!mIsResolving && mShouldResolve){
+            if(connectionResult.hasResolution()){
+                try{
+                    connectionResult.startResolutionForResult(this, RC_SIGN_IN);
+                    mIsResolving = true;
+                }catch(IntentSender.SendIntentException e){
+                    Log.e(TAG, "Could not resolve ConnectionResult.", e);
+                    mIsResolving = false;
+                    mGoogleApiClient.connect();
+                }
+            }else{
+                showErrorDialog(connectionResult);
+            }
+        }else{
+            showSignedOutUI();
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle){
+        Log.d(TAG, "onConnected: " + bundle);
+        mShouldResolve = false;
+        showSignedInUi();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.plus_sign_in_button){
+            onSignInClicked();
+        }
+    }
+
+    private void onSignInClicked() {
+        mShouldResolve = true;
+        mGoogleApiClient.connect();
+        //mStatusTextView.setText(R.string.signing_in);
+    }
+
+    protected abstract void showErrorDialog(ConnectionResult result);
+    protected abstract void showSignedOutUI();
+    protected abstract void showSignedInUi();
+}
