eaf41842d69ba746e7fff79c33f5ae611549cef9
Nan Montaño <nano.mv112@gmail.com>
2015-09-09 08:42:14 -0700

    gradle

--- app/src/main/java/mx/uson/cc/smed/PlusBaseActivity.java
+++ app/src/main/java/mx/uson/cc/smed/PlusBaseActivity.java
@@ -6,6 +6,7 @@ import android.content.IntentSender;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.Scopes;
